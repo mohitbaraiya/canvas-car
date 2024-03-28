c608e68a8292c79cb8d0233880fc48240412ec4d
mohitbaraiya <mohitbaraiya666@gmail.com>
2024-03-28 14:54:07 +0530

    fix canvas height issue

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,5 @@
 import { Car } from "./car.js";
 const canvas = document.getElementById("canvas");
-canvas.height = window.innerHeight;
 canvas.width = 300;
 const ctx = canvas.getContext("2d");
 
@@ -8,6 +7,7 @@ const car = new Car(150, 150, 30, 50);
 
 function animate() {
   car.update();
+  canvas.height = window.innerHeight;
   car.draw(ctx);
   requestAnimationFrame(animate);
 }
