83022f33b0f9f413784914c897fea63b18cbd9a5
mohitbaraiya <mohitbaraiya666@gmail.com>
2024-03-28 16:02:41 +0530

    add car movement

--- scripts/car.js
+++ scripts/car.js
@@ -7,10 +7,18 @@ export class Car {
     this.width = width;
     this.height = height;
     this.controls = new Controls(this.update);
+    this.speed = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+    this.angle = 0;
   }
 
   draw(ctx) {
     ctx.beginPath();
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
     /*
         first is x position
         second is y position
@@ -19,22 +27,58 @@ export class Car {
         of rectangle
     */
     console.log(JSON.stringify(this.controls));
-    ctx.rect(
-      this.x - this.width / 2,
-      this.y - this.height / 2,
-      this.width,
-      this.height
-    );
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
     ctx.fill();
+    ctx.restore();
   }
 
   //   update the car
   update() {
+    // set speed  for forward direction
     if (this.controls.forward) {
-      this.y -= 2;
+      this.speed += this.acceleration;
     }
+
+    // set speed for reverse direction
     if (this.controls.reverse) {
-      this.y += 2;
+      this.speed -= this.acceleration;
     }
+
+    // check for max forwards speed
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+
+    // check for max reverse speed
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+
+    // if speed is greater than 0 it reduce by friction
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+
+    // if speed is less than 0 it reduce by friction
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+    // move car by a speed in up direction
+
+    // move horizontal car by a speed in
+    if (this.controls.left && this.speed !== 0) {
+      this.angle += 0.03;
+    }
+    if (this.controls.right && this.speed !== 0) {
+      this.angle -= 0.03;
+    }
+
+    this.x -= Math.sin(this.angle) * this.speed;
+    this.y -= Math.cos(this.angle) * this.speed;
+    // this.y -= this.speed;
   }
 }
